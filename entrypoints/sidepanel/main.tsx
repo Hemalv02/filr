cefbc180d733e6e5342fa6dbf9689e3edae6ab78
Hemalv02 <mominulislam-2021411216@cs.du.ac.bd>
2025-10-13 03:19:53 +0600

    Configure ShadCN

--- entrypoints/sidepanel/main.tsx
+++ entrypoints/sidepanel/main.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './style.css';
+import '@/styles/globals.css';
+import { Button } from '@/components/ui/button';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Button onClick={() => {
+        console.log('Button clicked');
+        alert('Button clicked!');
+      }}>
+        Click me
+      </Button>
+    </div>
   </React.StrictMode>,
 );

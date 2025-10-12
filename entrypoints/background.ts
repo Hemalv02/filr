20ba90aefaf6b7e9c37f7e6df4dc41edbceeac77
Hemalv02 <mominulislam-2021411216@cs.du.ac.bd>
2025-10-13 03:04:09 +0600

    Initialize tailwindcss and sidepanel mode

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,3 +1,8 @@
 export default defineBackground(() => {
   console.log('Hello background!', { id: browser.runtime.id });
+
+  // Enable the side panel on all sites (we will restrict this later)
+  browser.sidePanel
+    .setPanelBehavior({ openPanelOnActionClick: true })
+    .catch((error) => console.error(error));
 });
--- wxt.config.ts
+++ wxt.config.ts
@@ -3,4 +3,10 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
+  manifest: {
+    name: 'Filr',
+    action: {
+      default_title: 'Open Filr Side Panel',
+    },
+  },
 });
